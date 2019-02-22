131f271df5e899687ae375604ba325e78da1681c
anagorny <anagorny@wv00124.local>
2019-02-22 17:30:44 +0300

    fix signatureHelp bug, update suggestions.json and add types in hover

--- server/src/LspService.ts
+++ server/src/LspService.ts
@@ -2,7 +2,7 @@ import {
     TextDocument, CompletionItemKind,
     Diagnostic, CompletionItem, Position, Range, DiagnosticSeverity, CompletionList, SignatureHelp
 } from "vscode-languageserver-types";
-import { globalSuggestions, types as fieldsMap, functions as funcsMap } from './suggestions/index'
+import { globalSuggestions, globalVariables, transactionClasses, types as fieldsMap, functions as funcsMap, types } from './suggestions/index'
 import { safeCompile } from './safeCompile'
 
 
@@ -37,56 +37,53 @@ export class LspService {
         const matchDeclarations = this.findMatchDeclarations(textBefore)
         let result: CompletionItem[] = [];
 
-        let nonTranzactionsClasses = ['Address', 'Alias', 'Transfer', 'DataEntry', 'GenesisTransaction', 'PaymentTransaction'];
-        let transactionClasses = Object.keys(fieldsMap).filter(val => nonTranzactionsClasses.indexOf(val) === -1)
-
         try {
             let wordBeforeDot = line.match(/([a-zA-z0-9_]+)\.[a-zA-z0-9_]*\b$/)     // get text before dot (ex: [tx].test)
 
             switch (true) {
                 case (character === '.' || wordBeforeDot !== null):                 //autocompletion after clicking on a dot
-                    
+
                     let inputWord = (wordBeforeDot === null)                        //get word before dot or last word in line
-                        ? line.match(/\b(\w*)\b\./g).pop().slice(0, -1) 
+                        ? line.match(/\b(\w*)\b\./g).pop().slice(0, -1)
                         : wordBeforeDot[1];
 
                     switch (true) {
-                        case (['buyOrder', 'sellOrder'].indexOf(inputWord) > -1): 
-                            result = fieldsMap['Order'];                            //ExchangeTransaction:'buyOrder', 'sellOrder'
+                        case (['buyOrder', 'sellOrder'].indexOf(inputWord) > -1):
+                            result = fieldsMap['Order'].fields;                            //ExchangeTransaction:'buyOrder', 'sellOrder'
                             break;
-                        case (['recipient'].indexOf(inputWord) > -1):               //'recipient'
-                            result = [...fieldsMap['Address'], ...fieldsMap['Alias']]; 
+                        case (['recipient'].indexOf(inputWord) > -1):               //Transfer:'recipient'
+                            result = [...fieldsMap['Address'].fields, ...fieldsMap['Alias'].fields];
                             break;
                         case (['tx'].indexOf(inputWord) > -1):                      // 'tx'
-                            result = intersection(...transactionClasses.map(val => fieldsMap[val])); 
+                            result = intersection(...transactionClasses.map(val => fieldsMap[val].fields));
                             break;
                         case ([...caseDeclarations].pop() === inputWord):           //case variable:
                             let temp = textBefore.match(/\bcase[ \t]*.*/g).pop()    //get line with last "case" block
                                 .match(new RegExp(                                  //search fields in line 
                                     `\\b${Object.keys(fieldsMap).join('\\b|\\b')}\\b`, 'g')
-                                    ) 
-                                .map(value => fieldsMap[value]);
+                                )
+                                .map(value => fieldsMap[value].fields);
                             result = intersection(...temp);
                             break;
                         default:
                             this.findLetDeclarations(textBefore).map((val, _, arr) => {
                                 if (val.name === inputWord) {
-                                    result = fieldsMap[val.value];
+                                    result = fieldsMap[val.value].fields;
                                     arr.length = 0;
                                 }
                             });
                             break;
                     }
                     break;
-                    //autocompletion after clicking on a colon or pipe
-                case ([':', '|'].indexOf(character) !== -1 || line.match(/([a-zA-z0-9_]+)[ \t]*[|:][ \t]*[a-zA-z0-9_]*$/) !== null): 
+                //autocompletion after clicking on a colon or pipe
+                case ([':', '|'].indexOf(character) !== -1 || line.match(/([a-zA-z0-9_]+)[ \t]*[|:][ \t]*[a-zA-z0-9_]*$/) !== null):
                     ([...matchDeclarations].pop() === 'tx')                         // if match(tx) else match(!tx)
-                        ? result = transactionClasses.map(val => ({ label: val, kind: CompletionItemKind.Class }))  
-                        : result = Object.keys(fieldsMap).map(val => ({ label: val, kind: CompletionItemKind.Class })); 
+                        ? result = transactionClasses.map(val => ({ label: val, kind: CompletionItemKind.Class }))
+                        : result = Object.keys(fieldsMap).map(val => ({ label: val, kind: CompletionItemKind.Class }));
                     break;
                 default:
                     result = [
-                    // get variables after "let" and globalSuggestions
+                        // get variables after "let" and globalSuggestions
                         ...getDataByRegexp(textBefore, /^[ \t]*let[ \t]+([a-zA-z][a-zA-z0-9_]*)[ \t]*=[ \t]*([^\n]+)/gm)
                             .map(val => ({ label: val.name, kind: CompletionItemKind.Variable })),
                         ...globalSuggestions
@@ -106,20 +103,29 @@ export class LspService {
     public hover(document: TextDocument, position: Position) {
         const word = getWordByPos(document.getText().split('\n')[position.line], position.character);
         let result = [];
-        if (funcsMap[word])
-        result.push(`**${word}** (\n
-            ${funcsMap[word].params.map((p:ParamType) => `\n * ${`${p.label}${p.required ? "":"?"}:`+
-                ` ${p.type} - ${p.description}`} \n`)}\n) : ${funcsMap[word].type} \n>_${funcsMap[word].doc}_`);
+        if (funcsMap[word]){
+            result.push(`**${word}** (\n
+                ${funcsMap[word].params.map((p: ParamType) => `\n * ${`${p.name}${p.required ? "" : "?"}:` +
+                ` ${p.type} - ${p.detail}`} \n`)}\n) : ${funcsMap[word].type} \n>_${funcsMap[word].detail}_`);
+        }else if(fieldsMap[word]){
+            result.push(fieldsMap[word].detail)
+        }else if(globalVariables[word]){
+            result.push(globalVariables[word].detail)
+        }
+            
         return {
             contents: result
         };
     }
 
 
-    public signatureHelp(document: TextDocument, position: Position) :SignatureHelp {
+    public signatureHelp(document: TextDocument, position: Position): SignatureHelp {
 
-        const offset = document.offsetAt( position );
-        const character = document.getText().substring( offset - 1, offset );
+        const offset = document.offsetAt(position);
+        const character = document.getText().substring(offset - 1, offset);
+        const textBefore = document.getText({ start: { line: 0, character: 0 }, end: position });
+        const lastFunction = textBefore.match(/\b([a-zA-z0-9_]*)\b[ \t]*\(/g); //get function calls
+        const functionArguments = textBefore.split(lastFunction.pop()).pop()
 
         let out: SignatureHelp = { //empty SignatureHelp
             activeParameter: null,
@@ -127,26 +133,26 @@ export class LspService {
             signatures: [],
         };
 
-        if ( character === ")" ) 
+        if (character === ")" || functionArguments.split(')').length > 1)
             return out;
 
         try {
-            const textBefore = document.getText( { start: { line: 0, character: 0 }, end: position } );
-            const lastFunction = textBefore.match( /\b([a-zA-z0-9_]*)\b[ \t]*\(/g ); //get function calls
-            const word = ( lastFunction.pop().slice( 0, -1) ); //get last function call
+            const word = (lastFunction.pop().slice(0, -1)); //get last function call
             let result = [];
-            if ( funcsMap[word] )
+            if (funcsMap[word])
                 result.push({
                     //get label in format: functionName (parameter1, parameter2 ...)
-                    label: `${word}(${funcsMap[word].params.map((p:ParamType) => `${p.label}${p.required ? "":"?"}:`+
+                    label: `${word}(${funcsMap[word].params.map((p: ParamType) => `${p.name}${p.required ? "" : "?"}:` +
                         ` ${p.type}`).join(', ')}): ${funcsMap[word].type}`,
-                    documentation: funcsMap[word].doc,
-                    parameters: funcsMap[word].params.map((p:ParamType) => ({ label: `${p.label}${p.required ? "":"?"}:`+
-                        ` ${p.type}`, documentation: p.description }))
+                    documentation: funcsMap[word].detail,
+                    parameters: funcsMap[word].params.map((p: ParamType) => ({
+                        label: `${p.name}${p.required ? "" : "?"}:` +
+                            ` ${p.type}`, documentation: p.detail
+                    }))
                 });
             out = {
                 //get comma`s count inside brackets
-                activeParameter: ( textBefore.split(lastFunction.pop()).pop().split(',').length - 1 ) ,
+                activeParameter: (functionArguments.split(',').length - 1),
                 activeSignature: 0,
                 signatures: result,
             };
@@ -162,8 +168,8 @@ export class LspService {
     private findLetDeclarations(text: string): LetDeclarationType[] {
         let rx = new RegExp(`\\b${Object.keys(fieldsMap).join('\\b|\\b')}\\b`, 'g');//this regexp looks for fields
         return [
-                    //this regexp looks for variables
-            ...getDataByRegexp(text, /^[ \t]*let[ \t]+([a-zA-z][a-zA-z0-9_]*)[ \t]*=[ \t]*([^\n]+)/gm)  
+            //this regexp looks for variables
+            ...getDataByRegexp(text, /^[ \t]*let[ \t]+([a-zA-z][a-zA-z0-9_]*)[ \t]*=[ \t]*([^\n]+)/gm)
         ].filter(val => {
             let match = val.value.match(rx)
             if (match !== null) {
@@ -203,10 +209,10 @@ interface LetDeclarationType {
 
 
 interface ParamType {
-    label: string
+    name: string
     type: string
     required: boolean
-    description: string
+    detail: string
 }
 
 function intersection(...args: any): CompletionItem[] {
--- server/src/suggestions/index.ts
+++ server/src/suggestions/index.ts
@@ -1,12 +1,34 @@
-import {CompletionItemKind, CompletionItem} from 'vscode-languageserver-types';
-const suggestions = require('../../src/suggestions/suggestions.json');
+import { CompletionItemKind, CompletionItem } from 'vscode-languageserver-types';
+// const suggestions = require('../../src/suggestions/suggestions.json');
+import * as suggestions from './suggestions.json'
 
-export const types = suggestions.types;
-export const functions = suggestions.functions;
+const nonTranzactionsClasses = ['Address', 'Alias', 'Transfer', 'DataEntry', 'GenesisTransaction', 'PaymentTransaction'];
+export const transactionClasses = Object.keys(suggestions.types).filter(val => nonTranzactionsClasses.indexOf(val) === -1)
+
+const typesData: Record<string, any>  = suggestions.types;
+
+Object.keys(typesData).map(type => {
+    typesData[type].label = type;
+    Object.keys(typesData[type].fields).map( field => {
+        typesData[type].fields[field].label = typesData[type].fields[field].name;
+        typesData[type].fields[field].kind = CompletionItemKind.Field
+    })
+})
+
+export const types: Record<string, any> = typesData;//(Object as any).values(suggestions.types)
+    // .map((x: any) => ({ ...x, label: x.name, fields: x.fields.map((x: any) => ({...x, label: x.name})) }));
+export const functions: Record<string, any> = suggestions.functions;
+export const globalVariables: Record<string, any> = suggestions.globalVariables;
+
 
 export const globalSuggestions: CompletionItem[] =
-    suggestions.keywords.map((label: string) => <CompletionItem>({label, kind: CompletionItemKind.Keyword}))
-        .concat(suggestions.generalSuggestions)
-        .concat(suggestions.cryptoFunctions)
-        .concat(suggestions.contextFunctions)
-        .concat(suggestions.contextFields);
+    suggestions.keywords.map((label: string) => <CompletionItem>({ label, kind: CompletionItemKind.Keyword }))
+        //globalVariables
+        .concat((Object as any).values(suggestions.globalVariables)
+            .map((x: any) => ({ ...x, kind: CompletionItemKind.Variable, label: x.name })))
+        //functions
+        .concat((Object as any).values(suggestions.functions)
+            .map((x: any) => ({ ...x, kind: CompletionItemKind.Function, label: x.name })))
+        //snippets
+        .concat((Object as any).values(suggestions.snippets)
+            .map((x: any) => ({ ...x, kind: CompletionItemKind.Snippet })))
