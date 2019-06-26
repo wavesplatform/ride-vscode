32841a47837814dcab33a7c16aba17ae370dd0cc
Alexey Nagorny <anagorny@wavesplatform.com>
2019-06-26 16:20:09 +0300

    add build tmLanguage script

---
+++ scripts/buildTmLanguage.ts
@@ -0,0 +1,30 @@
+const path = './syntaxes/ride.tmLanguage.json';
+const tmLanguage = require('.' + path);
+const fs = require('fs');
+const libVersion = 3;
+const funcsIndex = tmLanguage.patterns.findIndex(({name}) => name === 'entity.name.function.ride');
+const typesIndex = tmLanguage.patterns.findIndex(({name}) => name === 'entity.name.type.ride');
+
+import { getFunctionsDoc, getTypes } from '@waves/ride-js';
+
+tmLanguage.patterns[funcsIndex].match = `\\b(${
+    getFunctionsDoc(libVersion)
+        .filter(({name}) => ['*', '\\', '/', '%', '+',].indexOf(name) === -1)
+        .map(({name}) => name)
+        .join('|')
+    })\\b`;
+
+tmLanguage.patterns[typesIndex].match = `\\b(${
+    getTypes(libVersion).map(({name}) => name).join('|')
+    })\\b`;
+
+try {
+    fs.unlinkSync(path);
+} catch (e) {
+}
+
+fs.appendFile(path, (JSON.stringify(tmLanguage, null, 4)), function (err) {
+    if (err) throw err;
+    console.log('✅ -> tmLanguage were saved to ' + path);
+});
+
