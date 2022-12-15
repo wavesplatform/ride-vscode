37ff5aa4ec1b508022d766667ef25440d68e198f
veserion <lobanovdanil97@gmail.com>
2022-12-15 16:40:59 +0300

    fix paths for windows

--- server/src/utils/getLibURI.ts
+++ server/src/utils/getLibURI.ts
@@ -1,29 +1,29 @@
 import path = require("path");
 import {Files} from "vscode-languageserver";
 
-function search_largest_substr(){
+function search_largest_substr() {
     let str_min = arguments[0];
     const list = [];
-    for(let n=1; n<arguments.length; n++){
-        if(str_min.length<arguments[n].length){
+    for (let n = 1; n < arguments.length; n++) {
+        if (str_min.length < arguments[n].length) {
             list.push(arguments[n]);
             continue;
         }
         list.push(str_min);
         str_min = arguments[n];
     }
-    for(let l=str_min.length; l>0; l--){
+    for (let l = str_min.length; l > 0; l--) {
 
-        for(let p=0; p<=str_min.length-l; p++){
-            const substr = str_min.slice(p, p+l);
+        for (let p = 0; p <= str_min.length - l; p++) {
+            const substr = str_min.slice(p, p + l);
             let isFound = true;
-            for(let i=0; i<list.length; i++){
-                if(	list[i].indexOf(substr) >= 0)
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].indexOf(substr) >= 0)
                     continue;
-                isFound=false;
+                isFound = false;
                 break;
             }
-            if( isFound )
+            if (isFound)
                 return substr;
         }
     }
@@ -32,8 +32,9 @@ function search_largest_substr(){
 
 export const getLibURI = (uri: string, libPath: string): string => {
     const pathToLib = path.resolve(libPath)
+    console.log('pathToLib', pathToLib)
     // @ts-ignore
-    const subStr = search_largest_substr(uri, Files.uriToFilePath(uri))
+    const subStr = search_largest_substr(uri, (Files.uriToFilePath(uri) || '').replace(/\\/, '/'))
     const uriRoot = uri.replace(subStr, '')
     return uriRoot + pathToLib
 }
